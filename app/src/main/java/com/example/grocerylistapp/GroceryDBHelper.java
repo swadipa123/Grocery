cd14bdf1b480488baa0b21a2891765da6e9ab838
swadipa123 <40620169+swadipa123@users.noreply.github.com>
2020-09-29 19:39:53 +0530

    first commit 2part

---
+++ app/src/main/java/com/example/grocerylistapp/GroceryDBHelper.java
@@ -0,0 +1,42 @@
+package com.example.grocerylistapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.grocerylistapp.GroceryContract.*;
+
+import androidx.annotation.Nullable;
+
+public class GroceryDBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "grocerylist.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public GroceryDBHelper(@Nullable Context context) {
+
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        final String SQL_CREATE_GROCERYLIST_TABLE ="CREATE TABLE " +
+                GroceryEntry.TABLE_NAME + " (" +
+                GroceryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                GroceryEntry.COLUMN_NAME + " TEXT NOT NULL, " +
+                GroceryEntry.COLUMN_AMOUNT + " INTEGER NOT NULL, " +
+                GroceryEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                ");";
+
+        db.execSQL(SQL_CREATE_GROCERYLIST_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + GroceryEntry.TABLE_NAME);
+        onCreate(db);
+
+    }
+}
